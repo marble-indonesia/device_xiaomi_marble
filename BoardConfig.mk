38a39516f863f619b422c934dd3f38ef7c0ccb8b
Maulana Riaddy <maulriaddy@gmail.com>
2023-11-03 13:49:57 +0700

    marble: Go coming evox

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,8 +110,8 @@ BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
     kpti=off \
-    swinfo.fingerprint=$(AWAKEN_VERSION) \
-    mtdoops.fingerprint=$(AWAKEN_VERSION)
+    swinfo.fingerprint=$(EVOX_VERSION) \
+    mtdoops.fingerprint=$(EVOX_VERSION)
 
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
@@ -243,7 +243,7 @@ DEVICE_MANIFEST_UKEE_FILES := \
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/awaken/config/device_framework_matrix.xml
+    vendor/evox/config/device_framework_matrix.xml
 ODM_MANIFEST_SKUS += marble
 ODM_MANIFEST_MARBLE_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
 
--- evolution_marble.mk
+++ evolution_marble.mk
@@ -8,20 +8,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Awaken stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+# Inherit some common Evox stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
+
+EVO_BUILD_TYPE := UNOFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_FACE_UNLOCK_OPTOUT := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_ADAPTIVE_CHARGING := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 ## Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := awaken_marble
+PRODUCT_NAME := evolution_marble
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Derp
-TARGET_SUPPORTS_QUICK_TAP := true
