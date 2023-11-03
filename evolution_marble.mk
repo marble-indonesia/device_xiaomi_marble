8c743fd01a7d996771b8ebda54cb8ef711f9533d
Maulana Riaddy <maulriaddy@gmail.com>
2023-11-03 14:17:09 +0700

    marble: Add maintainer evox

--- evolution_marble.mk
+++ evolution_marble.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Evox stuff.
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
