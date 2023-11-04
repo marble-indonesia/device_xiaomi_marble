a6d368de0f97a6767bcd1d2a8dc9b29c46d4c6e9
mauls-xyz <maulriaddy23@gmail.com>
2023-11-04 08:59:34 +0700

    marble: Welcome pixelexperience

--- aosp_marble.mk
+++ aosp_marble.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evox stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
@@ -25,7 +25,7 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := true
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := evolution_marble
+PRODUCT_NAME := aosp_marble
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
