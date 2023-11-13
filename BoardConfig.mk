1a6e32fe50a75b0f5bf87feacc0545f0b5a62f65
Maulana Riaddy <maulriaddy@gmail.com>
2023-11-13 15:46:39 +0700

    marble: Welcome SOSP A14

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,8 +115,8 @@ BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
     kpti=off \
-    swinfo.fingerprint=$(AOSP_VERSION) \
-    mtdoops.fingerprint=$(AOSP_VERSION)
+    swinfo.fingerprint=$(SOSP_VERSION) \
+    mtdoops.fingerprint=$(SOSP_VERSION)
 
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
@@ -242,7 +242,7 @@ DEVICE_MANIFEST_UKEE_FILES := \
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    vendor/sosp/config/device_framework_matrix.xml
 ODM_MANIFEST_SKUS += marble
 ODM_MANIFEST_MARBLE_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
 
