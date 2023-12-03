a1fd775a7aa611c2a5772b8f77c1fe2d297040a9
mauls-xyz <maulriaddy23@gmail.com>
2023-12-03 10:38:25 +0700

    marble: Welcome mist os for marble a14

--- mist_marble.mk
+++ mist_marble.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Mist OS stuff.
+$(call inherit-product, vendor/mist/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
@@ -21,12 +21,17 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_ADAPTIVE_CHARGING := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_BUILD_VIMUSIC := true
 
 ## Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_marble
+PRODUCT_NAME := mist_marble
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Gapps
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
